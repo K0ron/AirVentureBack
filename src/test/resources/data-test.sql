b57612cdbf4ea92067f3d0e869e0b1638e58a531
Kevin Caron <kevin.caron.91@gmail.com>
2024-11-20 11:04:42 +0100

    comit for push into branch test

--- src/test/resources/data-test.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "users" (email, first_name, last_name, password, role)
-VALUES ('john@gmail.com', 'jhon', 'Doe', '$2a$12$S/VXri58yQkBCEIk9CnRtOSXZLFEa03dd5gJ5YwfqFH8wR6Lbfq8S', 'PARTICULAR');
-
-INSERT INTO "activity" (id, name, desciption, duration, city, adress, zip_code, price)
-VALUES (0, 'Parapente', 'Le parapente se pratique avec une voile', 1, 'Ascin','Rue de baigura', '64400', 100);
