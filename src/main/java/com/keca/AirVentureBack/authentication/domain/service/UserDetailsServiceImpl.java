4a676a7b584467f7ad447f1d4e450f5c0c858ca9
K0ron <kevin.caron.91@gmail.com>
2025-03-11 12:00:13 +0100

    prod OK

--- src/main/java/com/keca/AirVentureBack/authentication/domain/service/UserDetailsServiceImpl.java
+++ src/main/java/com/keca/AirVentureBack/authentication/domain/service/UserDetailsServiceImpl.java
@@ -11,18 +11,20 @@ import org.springframework.stereotype.*;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+
     private final UserRepository userRepository;
 
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+     @Override
+     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email);
         if (user == null) {
-            throw new UsernameNotFoundException("User not found with email: " + email);
+             throw new UsernameNotFoundException("User not found with email -> " + email);
         }
         return new UserPrincipal(user);
-    }
+     }
 }
--- src/main/java/com/keca/AirVentureBack/configuration/JpaConfig.java
+++ src/main/java/com/keca/AirVentureBack/configuration/JpaConfig.java
@@ -2,6 +2,7 @@ package com.keca.AirVentureBack.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -11,13 +12,16 @@ import org.springframework.transaction.PlatformTransactionManager;
 import jakarta.persistence.EntityManagerFactory;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.keca.AirVentureBack")
+@EnableJpaRepositories(basePackages = "com.keca.AirVentureBack", entityManagerFactoryRef = "jpaSharedEM_entityManagerFactory")
 @EntityScan(basePackages = "com.keca.AirVentureBack")
 public class JpaConfig {
 
@@ -33,8 +37,22 @@ public class JpaConfig {
     @Value("${spring.datasource.driver-class-name}")
     private String driverClassName;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String hibernateDdlAuto;
+
+    @Value("${spring.jpa.show-sql}")
+    private boolean showSql;
+
+    @Value("${spring.jpa.properties.hibernate.dialect}")
+    private String hibernateDialect;
+
     @Bean
+    @Primary
     public DataSource dataSource() {
+        System.out.println("Datasource URL: " + datasourceUrl);
+        System.out.println("Datasource Username: " + datasourceUsername);
+        System.out.println("Datasource Driver: " + driverClassName);
+        
         return DataSourceBuilder.create()
             .url(datasourceUrl)
             .username(datasourceUsername)
@@ -43,21 +61,24 @@ public class JpaConfig {
             .build();
     }
 
-    @Bean(name = "emf")
+    @Bean(name = "jpaSharedEM_entityManagerFactory")
+    @Primary
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        EntityManagerFactoryBuilder builder = new EntityManagerFactoryBuilder(
-                new HibernateJpaVendorAdapter(), null, null);
-        return builder
-                .dataSource(dataSource())
-                .packages("com.keca.AirVentureBack.authentication.domain.entity",
-                "com.keca.AirVentureBack.user.domain.entity",
-                "com.keca.AirVentureBack.activity.domain.entity",
-                "com.keca.AirVentureBack.reservation.domain.entity",
-                "com.keca.AirVentureBack.upload.domain.entity",
-                "com.keca.AirVentureBack.payment.domain.entity",
-                "com.keca.AirVentureBack.token.domain.entity")  
-                .persistenceUnit("default")
-                .build();
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPackagesToScan("com.keca.AirVentureBack");
+
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", hibernateDialect);
+        properties.setProperty("hibernate.hbm2ddl.auto", hibernateDdlAuto);
+        properties.setProperty("hibernate.show_sql", String.valueOf(showSql));
+        properties.setProperty("hibernate.format_sql", "true");
+
+        factoryBean.setJpaProperties(properties);
+        factoryBean.setPersistenceUnitName("default");
+        return factoryBean;
     }
 
     @Bean
@@ -65,3 +86,5 @@ public class JpaConfig {
         return new JpaTransactionManager(entityManagerFactory);
     }
 }
+
+
